c1b04227eecf09a08fc887efb0363bd1292caa86
Brian <riceb53@gmail.com>
2019-06-23 10:07:47 -0500

    first route

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,6 +1,6 @@
 class Api::ExamplePagesController < ApplicationController
   def the_good_morning_method
     # response
-    # render json: {message: "good morning"}
+    render json: {message: "good morning"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # namespace :api do
-  #   # route (chrome)             controller        method name (goes in controller)
-  #   get '/good_morning' => 'example_pages#the_good_morning_method'
-  # end
+  namespace :api do
+    # route (chrome)             controller        method name (goes in controller)
+    get '/good_morning' => 'example_pages#the_good_morning_method'
+  end
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   # namespace :api do
